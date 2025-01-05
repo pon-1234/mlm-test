aac2dc877977c8d9b11eb00e6e4a9ab7d5e49cee
pon <mamepon@gmail.com>
2025-01-05 11:04:46 +0900

    chore: Remove frontend and backend directories

--- backend/src/libs/Response.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from 'express';
-
-export const returnSuccess = (res: Response, data: any = [], message: string = 'Success', code: number = 200) => {
-    res.status(code).json({ title: message, status: code, detail: data });
-};
-
-export const returnError = (res: Response, data: any = [], message: string = 'Error', code: number = 500,) => {
-    res.status(code).json({ title: message, code, detail: data });
-};
--- frontend/src/types/common.d.ts
+++
@@ -1,3 +0,0 @@
-interface UserLoginData {
-    token: string;
-}
